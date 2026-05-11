d1be89de5ca9c955375381f2ff2054682f821edc
Shobhini-stack <shobhini@campx.in>
2026-05-11 15:34:15 +0530

    feat: add routing with protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,43 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import ProtectedRoute from './routes/ProtectedRoute'
+import Login from './pages/Login'
+import Setup from './pages/Setup'
+import Home from './pages/Home'
+import Preview from './pages/Preview'
+
 export default function App() {
-  return <div className="p-4 text-xl">Greetings App</div>
+  return (
+    <>
+      <Toaster position="top-center" />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/setup"
+          element={
+            <ProtectedRoute>
+              <Setup />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/preview/:id"
+          element={
+            <ProtectedRoute>
+              <Preview />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </>
+  )
 }
